4281a05149f1a3f695bfac1d4b1d4dcdbb5755a3
User <User@DESKTOP-L130AHQ>
2023-03-05 11:49:10 +0000

    add class

---
+++ Hospital Management/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital_Management
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Address { get; set; }
+        public int ContactNo { get; set; }
+        public DateTime Birthdate { get; set; }
+    }
+}
--- Hospital Management/Program.cs
+++ Hospital Management/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿class Program
+{
+    public static void Main()
+    {
+        Welcome();
+    }
+
+    public void Welcome()
+    {
+        Console.WriteLine("Welcome");
+    }
+}
